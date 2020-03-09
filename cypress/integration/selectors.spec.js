e11de4fb8309d5a06037d98f0aabd41a624cf2fa
Volodymyr Maslo <Vladimirmaslo90@gmail.com>
2020-03-09 13:01:56 +0200

    Locators examples
    Locators examples test

---
+++ cypress/integration/selectors.spec.js
@@ -0,0 +1,39 @@
+/// <reference types="cypress" />
+
+describe('Our first suite', () => {
+  it('first test', () => {
+
+    cy.visit('/')
+    cy.contains('Forms').click()
+    cy.contains('Form Layouts').click()
+    //by Tag name
+    cy.get('input')
+
+    //by Id
+    cy.get('#inputEmail')
+
+    //by Class name
+    cy.get('.input-full-width')
+
+    //by attribute name
+    cy.get('[placeholder]');
+
+    //by attribute name and value
+    cy.get('[placeholder="Email"]');
+
+    //by class Value
+    cy.get('[class="input-full-width size-medium shape-rectangle"]')
+
+    //by tag name and attribute and value
+    cy.get('input[placeholder="Email"]')
+
+    //by 2 different attributes
+    cy.get('[placeholder="Email"][type="Email"]')
+
+    // by tag name, attribute with valu, id and class name
+    cy.get('input[placeholder="Email"]#inputEmail1.input-full-width')
+
+    //the most recommended way 
+    cy.get('[data-cy="imputEmail1"]');
+  })
+})
