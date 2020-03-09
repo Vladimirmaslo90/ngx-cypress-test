6f3b3441db232a4a8e066c3df31b5304bb52bc9e
Volodymyr Maslo <Vladimirmaslo90@gmail.com>
2020-03-09 13:39:31 +0200

    Test 2
    More comples way of getting elements.

--- cypress/integration/selectors.spec.js
+++ cypress/integration/selectors.spec.js
@@ -36,4 +36,25 @@ describe('Our first suite', () => {
     //the most recommended way 
     cy.get('[data-cy="imputEmail1"]');
   })
+  it.only('second test', () => {
+    cy.visit('/')
+    cy.contains('Forms').click()
+    cy.contains('Form Layouts').click()
+
+    cy.get('[data-cy="signInButton"]');
+    cy.contains('Sign in');
+    cy.contains('[status="warning"]', 'Sign in');
+
+    cy.get('#inputEmail3')
+      .parents('form')
+      .find('button')
+      .should('contain', 'Sign in')
+      .parents('form')
+      .find('nb-checkbox')
+      .click()
+
+    cy.contains('nb-card', 'Horizontal form').find('[type="Email"]')
+
+
+  })
 })
